e05e005b5c5199c5be26f29ae8cad3afda68674f
Becky Smith <rebkwok@gmail.com>
2016-07-20 23:05:55 +0100

    Second mockup based on lightwave

--- pole_performance.py
+++ pole_performance.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def homepage():
+@app.route("/pp1")
+def shield_homepage():
     return render_template('shield_home.html')
 
+
+@app.route("/pp2")
+def lightwave_homepage():
+    return render_template('lightwave_home.html')
 
 
 if __name__ == "__main__":
